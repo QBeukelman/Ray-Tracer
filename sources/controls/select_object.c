3c9963c209a3bdfb5cdf50dfbcaa37c915dd7fd5
QBeukelman <quentin.nextresearch@gmail.com>
2025-05-05 21:50:14 +0200

    feature/#68 Norminette complete

--- sources/controls/select_object.c
+++ sources/controls/select_object.c
@@ -6,7 +6,7 @@
 /*   By: quentinbeukelman <quentinbeukelman@stud      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/28 13:12:30 by quentinbeuk   #+#    #+#                 */
-/*   Updated: 2025/05/05 21:41:22 by quentinbeuk   ########   odam.nl         */
+/*   Updated: 2025/05/05 21:48:39 by quentinbeuk   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,12 @@ void	anounce_selection_and_instructions(t_scene *scene)
 	printf("Select object:\n");
 	printf("\t`[` Previous object.\n");
 	printf("\t`]` Next object.\n");
-
 	printf("\nSelect edit property:\n");
 	printf("\t`tab` Select next edit property.\n");
-
 	printf("\nSelect vecor:\n");
 	printf("\t`x` Select x vector.\n");
 	printf("\t`y` Select y vector.\n");
 	printf("\t`z` Select z vector.\n");
-
 	printf("\nAdjust property value:\n");
 	printf("\t`↑` Increment value.\n");
 	printf("\t`↓` Increment value.\n");
